bc32e8e8ee0785783d155c54222431e94e99b457
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 11:56:46 +0530

    Fetched

---
+++ EvenOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    class EvenOdd
+    {
+        public void findEvenOdd()
+        {
+            Console.WriteLine("Enter number to find even or odd");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is a even number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is a odd number");
+            }
+        }
+    }
+}
