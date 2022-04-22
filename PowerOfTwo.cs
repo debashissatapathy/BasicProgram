74d06ab048e849330706e96af71ab42ec3357344
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 12:18:18 +0530

    Fetched

---
+++ PowerOfTwo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    class PowerOfTwo
+    {
+        public void twoThePower()
+        {
+            Console.Write("Enter the value to get Power of two\n");
+            int num = Convert.ToInt16(Console.ReadLine());
+            int power = Convert.ToInt16(Math.Pow(2, num));
+            for (int i = 0; i <= power; i++)
+            {
+                Console.WriteLine("power of " + i + " is " + (Math.Pow(i, 2)));
+            }
+
+        }
+    }
+}
