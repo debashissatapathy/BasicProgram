1e69433b08309563420c4c3bfbe2ee54cd461677
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 12:25:41 +0530

    Fetched

---
+++ Quotient_Remainder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    class QuotientAndRemainder
+    {
+        public void chk_Qut_Remdr()
+        {
+            int a, b;
+            Console.WriteLine("Enter the number to find quotient and Remainder: ");
+            Console.WriteLine("Enter a value: ");
+            a = int.Parse(Console.ReadLine() + "\n");
+            Console.WriteLine("Enter b value: ");
+            b = int.Parse(Console.ReadLine() + "\n");
+            double num = Convert.ToDouble(a / b);
+            Console.WriteLine("Quotient of " + a + " & " + b + " is " + num + "\n\n");
+
+
+
+            int num1 = Convert.ToInt16(a % b);
+            Console.WriteLine("Remainder of " + a + " & " + b + " is " + num1 + "\n\n");
+        }
+    }
+}
