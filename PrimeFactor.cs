65347fb6bb83238cc86add7b805bdbbc3d6a2075
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 12:21:49 +0530

    Fetched

---
+++ PrimeFactor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    class PrimeFactor
+    {
+        public void primeFactor()
+        {
+            int n, i;
+            Console.WriteLine("Enter the number to find prime fnctor: ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i + " is prime factor of " + n);
+
+                }
+            }
+        }
+    }
+}
