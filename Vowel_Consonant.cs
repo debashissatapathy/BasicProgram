3bc993e9d453e0c4fd61014722e7c4686c999a0a
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 12:32:28 +0530

    Fetched

---
+++ Vowel_Consonant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    class VowelAndConsonant
+    {
+        public void chkAlphabet()
+        {
+            Console.WriteLine("Check whether alphabet is vowel or consonant\n");
+            Console.WriteLine("Enter an alphabet value\n");
+            char ch = Convert.ToChar(Console.ReadLine().ToLower());
+
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("a is a vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("e is a vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("i is a vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("o is a vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("u is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("Given alphabate is a consonant");
+                    break;
+            }
+
+        }
+    }
+}
