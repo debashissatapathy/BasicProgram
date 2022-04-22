d9cc1c0d0fb2ef1c94a0eb9a74d28347e4148f41
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-22 12:12:15 +0530

    Fetched

---
+++ LeapYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    class FindLeapYear
+    {
+        public void leapYear()
+        {
+            Console.WriteLine("Enter year below: ");
+            int year = Convert.ToInt16(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+                Console.WriteLine(year + " is not a leap year");
+        }
+    }
+}
